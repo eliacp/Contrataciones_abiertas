0ac04bfa46041dcbeda07639112c62cc5e121b63
Mario Torres <mario291187@gmail.com>
2017-12-04 13:42:45 -0600

    Parties button

--- Contrataciones_abiertas/captura/routes/index.js
+++ Contrataciones_abiertas/captura/routes/index.js
@@ -323,7 +323,7 @@ router.get('/main/:contractingprocess_id', isAuthenticated, function (req,res) {
 // NUEVO PROCESO DE CONTRATACIÓN
 router.post('/new-process', isAuthenticated, function (req, res) {
     db_conf.edca_db.tx(function (t) {
-        return t.one("insert into ContractingProcess (fecha_creacion, hora_creacion, ocid, stage ) values (current_date, current_time, concat('NUEVA_CONTRATACION_', current_date,'_', current_time), 0) returning id")
+        return t.one("insert into ContractingProcess (fecha_creacion, hora_creacion, ocid, stage ) values (current_date, current_time, concat('CONTRATACION_', current_date,'_', current_time), 0) returning id")
             .then(function (process) {
 
                 return t.batch([
