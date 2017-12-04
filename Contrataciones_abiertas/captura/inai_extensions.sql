0dee08461d391240798a419c29573407855e6d26
Mario Torres <mario291187@gmail.com>
2017-12-04 11:59:03 -0600

    db schema

--- Contrataciones_abiertas/captura/inai_extensions.sql
+++ Contrataciones_abiertas/captura/inai_extensions.sql
@@ -1,12 +1,70 @@
-create table request_for_quotes
 
-create table request_for_quotes_items 
+/***
+ * OCDS 1.1
+ ***/
 
-create table request_for_quotes_possible_suppliers 
 
-create table quotes
 
-create table quotes_items
+/***
+ * RequestForQuotes
+ ***/
 
-create table clarification_meeting
+drop table if exists RequestForQuotes cascade;
+create table RequestForQuotes(
+id serial primary key
+title text,
+description text,
+period_startdate timestamp,
+period_enddate timestamp,
+);
+
+drop table if exists RequestForQuotesItems cascade;
+create table RequestForQuotesItems(
+id serial primary key,
+requestforquotes_id integer references RequestForQuotes(id) on delete cascade
+/*incluir campos*/
+);
+
+drop table ir exists RequestForQuotesPossibleSuppliers cascade;
+create table RequestForQuotesPossibleSuppliers(
+id serial primary key,
+requestforquotes_id integer references RequestForQuotes(id) on delete cascade,
+parties_id integer references Parties(id)
+);
+
+drop table Quotes cascade;
+create table Quotes(
+id serial primary key,
+requestforquotes_id integer references RequestForQuotes(id) on delete cascade
+/* incluir campos */
+);
+
+drop table QuotesItems cascade;
+create table QuotesItems(
+id serial primary key,
+quotes_id integer references Quotes(id) on delete cascade
+/*incluir campos*/
+);
+
+/***
+ * ClarificationMeetings
+ ***/
+
+drop table ClarificationMeeting cascade;
+create table ClarificationMeeting(
+id serial primary key,
+date timestamp
+);
+
+create table ClarificationMeetingAttendees(
+id serial primary key,
+clarificationmeeting_id integer references ClarificationMeeting(id) on delete cascade,
+parties_id integer references Parties(id)
+);
+
+create table ClarificationMeetingOfficials(
+id serial primary key,
+clarificationmeeting_id integer references ClarificationMeeting(id) on delete cascade,
+parties_id integer references Parties(id)
+);
 
